d005132bd0df830cc19e6922f0b68478a3a68b55
palexeyeva <53582153+palexeyeva@users.noreply.github.com>
2021-09-19 21:07:50 +0300

    Добавление функций сохранения и чтения файла

--- Mimo/Mimo/settings.py
+++ Mimo/Mimo/settings.py
@@ -127,7 +127,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
+# )
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    '/Mimo/riddles/static/',
 )
--- Mimo/riddles/static/scripts/external.js
+++ Mimo/riddles/static/scripts/external.js
@@ -223,6 +223,167 @@ function validateForm(input) {
 function showGraph() {
   return false;
 }
+// Функция автоматического заполнения 
+function readFile(input) {
+
+  let file = input.files[0];
+
+  let reader = new FileReader();
+
+  reader.readAsText(file);
+
+  reader.onload = function() {
+    console.log(reader.result);
+    let res = reader.result;
+    let count = res.split(';')[0];
+    let iter = res.split(';')[1];
+
+    console.log(count);
+
+    document.getElementById("cellCount").value = count;
+    document.getElementById("iterCount").value = iter;
+    openTab("inputData");
+
+    let matrix = res.split(';')[2];
+    let p = -1;
+    for (i = 0; i < count; i++) {
+      for (j = 0; j < count; j++) {
+        n = 'm_'+(i+1)+'_'+(j+1);
+        p+=1;
+        document.getElementById(n).value = matrix.split(',')[p];        
+      }
+    }
+
+    let th = res.split(';')[3];
+    for (i = 0; i < count; i++) {
+      n = 'th_' + i;
+      document.getElementsByName(n)[0].value = th.split(',')[i];
+    }
+
+    let mem = res.split(';')[4];
+    for (i = 0; i < count; i++) {
+      n = 'mem_' + i;
+      document.getElementsByName(n)[0].value = mem.split(',')[i];
+    }
+
+    let disc = res.split(';')[5];
+    for (i = 0; i < count; i++) {
+      n = 'disc_' + i;
+      document.getElementsByName(n)[0].value = disc.split(',')[i];
+    }
+
+    let init = res.split(';')[6];
+    for (i = 0; i < count; i++) {
+      n = 'init_' + i;
+      document.getElementsByName(n)[0].value = init.split(',')[i];
+    }
+
+    let st = res.split(';')[7];
+    p = -1;
+    for (i = 0; i < count; i++) {
+      for (j = 0; j < 2; j++) {
+        n = 'st_'+(i+1)+'_'+(j+1);
+        p+=1;
+        document.getElementById(n).value = st.split(',')[p];        
+      }
+    }
+
+  };
+
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+
+
+}
+
+// Функция сохранение в БД 
+function saveFile() {
+  var str;
+  count = document.getElementById("cellCount").value;
+  str = String(count);
+  str += ';'
+  str += String(document.getElementById("iterCount").value);
+  str += ';'
+  for (i=0; i < count; i++) {
+    for (j=0; j<count; j++) {
+      n = 'm_'+(i+1)+'_'+(j+1);
+      str += String(document.getElementById(n).value);
+      if (i != (count-1) || j!=(count - 1)) {
+        str += ','
+      }      
+    }
+  }
+  str += ';';
+
+  for (i = 0; i < count; i++) {
+    n = 'th_' + i;
+    str += String(document.getElementsByName(n)[0].value);
+    if (i != (count-1)) {
+      str += ','
+    }   
+  }
+  str += ';'
+
+  for (i = 0; i < count; i++) {
+    n = 'mem_' + i;
+    str += String(document.getElementsByName(n)[0].value);
+    if (i != (count-1)) {
+      str += ','
+    }   
+  }
+  str += ';'
+
+  for (i = 0; i < count; i++) {
+    n = 'disc_' + i;
+    str += String(document.getElementsByName(n)[0].value);
+    if (i != (count-1)) {
+      str += ','
+    }   
+  }
+  str += ';'
+
+  for (i = 0; i < count; i++) {
+    n = 'init_' + i;
+    str += String(document.getElementsByName(n)[0].value);
+    if (i != (count-1)) {
+      str += ','
+    }   
+  }
+  str += ';';
+
+
+  for (i=0; i < count; i++) {
+    for (j=0; j<count; j++) {
+      n = 'st_'+(i+1)+'_'+(j+1);
+      str += String(document.getElementById(n).value);
+      if (i != (count-1) || j!=(count - 1)) {
+        str += ','
+      }      
+    }
+  }
+  str += ';';
+
+
+
+  console.log(str);
+  var a = document.createElement('a'); 
+  a.style.display = "none";
+  a.id = 'a';
+  document.getElementById("btns").appendChild(a);
+  type = "text/plain";
+  var file = new Blob([str], {type: type});
+  a.href = URL.createObjectURL(file);
+
+  nm = document.getElementById("FileName").value;  
+
+  a.download = nm;
+  a.click();
+
+  
+} 
+
+
 
 // Функция сохранения данных
 function downloadData() {
